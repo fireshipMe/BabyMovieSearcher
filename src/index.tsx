2c3ef27fbb8572ac63dc3b8bada7ecf78d941901
FireshipMe <mqclane@gmail.com>
2020-04-18 18:45:08 +0300

    Basic Redux added

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import { MovieInfo } from './components/movie-info'
-import { SearchMovies } from './components/search-movies'
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { MovieInfo } from './components/movie-info';
+import { SearchMovies } from './components/search-movies';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <React.Fragment>
-          <Switch>
-            <Route path="/:id" exact component={MovieInfo}></Route>
-          </Switch>
-          <SearchMovies />
-        </React.Fragment>
-      </BrowserRouter>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route path="/:id" exact component={MovieInfo}></Route>
+        </Switch>
+        <SearchMovies />
+      </React.Fragment>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
